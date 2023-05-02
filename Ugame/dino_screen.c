174bd3d74282b13924bfe2c525b88988f55ca1b9
Odangg <ss4188@naver.com>
2023-05-02 15:22:58 +0900

    ss4188, 커밋 내용 실습

---
+++ Ugame/dino_screen.c
@@ -0,0 +1,36 @@
+#include "dino_screen.h"
+
+int print_title_screen()
+{
+	printf("##############################################");
+	printf("##                                          ##");
+	printf("##                                          ##");
+	printf("##              Dino Run V.01               ##");
+	printf("##                                          ##");
+	printf("##                                          ##");
+	printf("##                                          ##");
+	printf("##                                          ##");
+	printf("##       1. Game Start                      ##");
+	printf("##       2. Help                            ##");
+	printf("##       3. Exit                            ##");
+	printf("##                                          ##");
+	printf("##                                          ##");
+	printf("##                                          ##");
+	printf("##                                          ##");
+	printf("##############################################");
+}
+
+int help_menu()
+{
+	printf("###################################");
+	printf("##                               ##");
+	printf("##  It is a game where you avoid ##");
+	printf("##                               ##");
+	printf("##                               ##");
+	printf("##  oncoming obstacles with the  ##");
+	printf("##                               ##");
+	printf("##                               ##");
+	printf("##       with the arrow keys     ##");
+	printf("##                               ##");
+	printf("###################################");
+}
---
+++ Ugame/dinorun.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int print_title_screen();
+
+int help_menu();
+
